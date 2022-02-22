2deb9bf5b37df61fecd6e47adc06e6742babb507
lucascaggiante <79833962+lucascaggiante@users.noreply.github.com>
2022-02-22 13:41:36 +0100

    Update YelpDao.java

--- src/main/java/it/polito/tdp/yelp/db/YelpDao.java
+++ src/main/java/it/polito/tdp/yelp/db/YelpDao.java
@@ -4,8 +4,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.javadocmd.simplelatlng.LatLng;
+import com.javadocmd.simplelatlng.LatLngTool;
+import com.javadocmd.simplelatlng.util.LengthUnit;
+
+import it.polito.tdp.yelp.model.ArcoGrafo;
 import it.polito.tdp.yelp.model.Business;
 import it.polito.tdp.yelp.model.Review;
 import it.polito.tdp.yelp.model.User;
@@ -110,6 +117,110 @@ public class YelpDao {
 			return null;
 		}
 	}
+
+	public List<String> getAllCities() {
+		String sql = "SELECT DISTINCT city\n"
+				+ "FROM business\n"
+				+ "ORDER BY city ";
+		List<String> result = new ArrayList<String>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				
+				
+				result.add(res.getString("city"));
+			}
+			res.close();
+			st.close();
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public List<Business> getBusinessByCity(String city) {
+		String sql = "SELECT *\n"
+				+ "FROM business\n"
+				+ "WHERE business.city = ?";
+		List<Business> result = new ArrayList<Business>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, city);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				Business business = new Business(res.getString("business_id"), 
+						res.getString("full_address"),
+						res.getString("active"),
+						res.getString("categories"),
+						res.getString("city"),
+						res.getInt("review_count"),
+						res.getString("business_name"),
+						res.getString("neighborhoods"),
+						res.getDouble("latitude"),
+						res.getDouble("longitude"),
+						res.getString("state"),
+						res.getDouble("stars"));
+				result.add(business);
+				
+				
+			}
+			res.close();
+			st.close();
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 	
+	public List<ArcoGrafo> calcolaArchi(String city) {
+		String sql = "SELECT b1.business_id as id1, b2.business_id as id2, b1.latitude AS x1, b1.longitude AS y1, b2.latitude AS x2, b2.longitude AS y2\n"
+				+ "FROM business b1, business b2\n"
+				+ "WHERE b1.business_id > b2.business_id\n"
+				+ "	AND b1.city = b2.city\n"
+				+ "	AND b1.city = ?" ;
+		
+		List<ArcoGrafo> result = new ArrayList<>() ;
+		
+		Connection conn = DBConnect.getConnection() ;
+		
+		try {
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			st.setString(1, city);
+			ResultSet res = st.executeQuery() ;
+			while(res.next()) {
+				Double x1 = res.getDouble("x1");
+				Double y1 = res.getDouble("y1");
+				Double x2 = res.getDouble("x2");
+				Double y2 = res.getDouble("y2");
+				
+				Double peso = LatLngTool.distance(new LatLng(x1,y1), 
+						new LatLng(x2, y2), 
+						LengthUnit.KILOMETER);
+				
+				result.add(new ArcoGrafo(
+						res.getString("id1"), 
+						res.getString("id2"), 
+						peso)) ;
+			}
+			conn.close();
+			return result ;
+		} catch(SQLException ex) {
+			throw new RuntimeException("DB Error", ex) ;
+		}
+	}
 	
 }
